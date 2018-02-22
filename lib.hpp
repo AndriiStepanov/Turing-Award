33ac0c495c448b4909c5f6676048a8cd9645b39d
Denys Smirnov <dendi239@gmail.com>
2018-02-22 20:48:32 +0200

    Updated lib.h — now it isn't using std as primary namespace.

--- lib.hpp
+++ lib.hpp
@@ -2,31 +2,113 @@
 #define LIB
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx")
-#include <bits/stdc++.h>
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
 
-using namespace std;
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+    // C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
 
 typedef long double ld;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef unsigned int uint;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
+typedef std::pair<int, int> pii;
+typedef std::pair<ll, ll> pll;
 inline double clocks() { return clock() * 1.0 / CLOCKS_PER_SEC; }
 
 template<class T1, class T2>
-ostream &operator<<(ostream &out, const pair<T1, T2> &a) {
+std::ostream &operator<<(std::ostream &out, const std::pair<T1, T2> &a) {
     out << "(" << a.first << "," << a.second << ")";
     return out;
 }
 
 template<class T1, class T2>
-istream& operator >> (istream& in, pair<T1, T2>& p) {
+std::istream& operator >> (std::istream& in, std::pair<T1, T2>& p) {
     return in >> p.first >> p.second;
 }
 
 template<class T1, class T2>
-ostream &operator<<(ostream &out, const vector<T1, T2> &a) {
+std::ostream &operator<<(std::ostream &out, const std::vector<T1, T2> &a) {
     bool f = 0;
     out << "[";
     for (auto &x : a) {
@@ -39,7 +121,7 @@ ostream &operator<<(ostream &out, const vector<T1, T2> &a) {
 }
 
 template<class T1, class T2>
-istream& operator >> (istream& in, vector<T1, T2>& a) {
+std::istream& operator >> (std::istream& in, std::vector<T1, T2>& a) {
     for (int i = 0; i < a.size(); i++) {
         in >> a[i];
     }
@@ -47,7 +129,7 @@ istream& operator >> (istream& in, vector<T1, T2>& a) {
 }
 
 template<class T1, class T2>
-ostream &operator<<(ostream &out, const set<T1, T2> &a) {
+std::ostream &operator<<(std::ostream &out, const std::set<T1, T2> &a) {
     bool f = 0;
     out << "{";
     for (auto &x : a) {
@@ -60,7 +142,7 @@ ostream &operator<<(ostream &out, const set<T1, T2> &a) {
 }
 
 template<class T1, class T2>
-ostream &operator<<(ostream &out, const multiset<T1, T2> &v) {
+std::ostream &operator<<(std::ostream &out, const std::multiset<T1, T2> &v) {
     bool f = 1;
     out << "[";
     for (auto &i : v) {
@@ -72,7 +154,7 @@ ostream &operator<<(ostream &out, const multiset<T1, T2> &v) {
 }
 
 template<class T1, class T2>
-ostream &operator<<(ostream &out, priority_queue<T1, T2> v) {
+std::ostream &operator<<(std::ostream &out, std::priority_queue<T1, T2> v) {
     bool f = 1;
     out << "[";
     while (!v.empty()) {
@@ -86,7 +168,7 @@ ostream &operator<<(ostream &out, priority_queue<T1, T2> v) {
 }
 
 template<class T1, class T2, class T3>
-ostream &operator<<(ostream &out, const map<T1, T2, T3> &a) {
+std::ostream &operator<<(std::ostream &out, const std::map<T1, T2, T3> &a) {
     bool f = 0;
     out << "{";
     for (auto &x : a) {
@@ -99,7 +181,7 @@ ostream &operator<<(ostream &out, const map<T1, T2, T3> &a) {
 }
 
 template<class T1, class T2>
-ostream &operator<<(ostream &out, const multimap<T1, T2> &v) {
+std::ostream &operator<<(std::ostream &out, const std::multimap<T1, T2> &v) {
     bool f = 1;
     out << "[";
     for (auto &ii : v) {
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "task.hpp"
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    solve(std::cin, std::cout);
+
+    return 0;
+}
