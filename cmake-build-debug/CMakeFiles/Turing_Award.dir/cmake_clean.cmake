064d912184dbe258f614de6d459ae6407ebc4c14
Andrii Stepanov <stepa1024@gmail.com>
2018-02-22 18:18:10 +0200

    r

---
+++ cmake-build-debug/CMakeFiles/Turing_Award.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Turing_Award.dir/main.cpp.o"
+  "Turing_Award.pdb"
+  "Turing_Award"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Turing_Award.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Turing_Award.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Turing_Award.
+# This may be replaced when dependencies are built.
