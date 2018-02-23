5bba0ea95a835b42c911aff36c9ddc22418158cd
Andrii Stepanov <stepa1024@gmail.com>
2018-02-23 15:01:49 +0200

    -

--- config.hpp
+++ config.hpp
@@ -4,6 +4,9 @@
 #include "test_lib.hpp"
 #include "sources/A.hpp"
 #include "sources/B.hpp"
+#include "sources/C.hpp"
+#include "sources/D.hpp"
+#include "sources/E.hpp"
 
 vector<problem> problems {
         {
@@ -15,10 +18,31 @@ vector<problem> problems {
         },
         {
                 "election",
-                run_option::run,
-                io_option::console,
+                run_option::skip,
+                io_option::file,
                 {{1, 10}},
                 solve_b
+        },
+        {
+                "crossword",
+                run_option::skip,
+                io_option::file,
+                {{1, 10}},
+                solve_c
+        },
+        {
+                "legacy",
+                run_option::skip,
+                io_option::file,
+                {{1, 10}},
+                solve_d
+        },
+        {
+                "rocks",
+                run_option::skip,
+                io_option::file,
+                {{1, 10}},
+                solve_e
         },
 };
 #endif
